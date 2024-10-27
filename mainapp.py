504a3cdaa601ad0b7d6344173f79f7f4df6d1be0
Luthfi-Bafana <luthfi@outlook.sg>
2024-10-27 12:09:05 +0800

    add disclaimer

--- mainapp.py
+++ mainapp.py
@@ -70,6 +70,20 @@ if st.button("Submit Question"):
     st.write("Response:")
     st.write(response)
 
+with st.expander("Disclaimer"):
+    st.write(
+        """
+
+    IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+
+    Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+
+    Always consult with qualified professionals for accurate and personalized advice.
+
+    """
+
+    )
+
 # mainapppy-rehvfzi4kea2ssbdp5qczv
 #financial-programs-app.streamlit.app
 # financial-programs-info.streamlit.app
